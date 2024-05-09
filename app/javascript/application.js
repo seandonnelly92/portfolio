da49c2510f3ba763f4e5c286f1a240ba8ef794b6
Sean Donnelly <sean_donnelly@hotmail.co.uk>
2024-05-09 08:51:36 +0100

    Corrected javascript folder file structure

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,12 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "@popperjs/core"
-import "bootstrap"
+import { Application } from "@hotwired/stimulus"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
+// Start Stimulus appliation
+const application = Application.start()
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+eagerLoadControllersFrom("controllers", application)
+
+export { application }
